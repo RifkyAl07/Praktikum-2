31ac1852c6d3b4e89251aa81f759807ab0ceac18
RifkyAl07 <92801053+RifkyAl07@users.noreply.github.com>
2021-11-03 21:18:15 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Praktikum-2
+# praktikum2
+
+## Mencari Keliling dan luas lingkaran
+ini adalah tampilannya
+![Gambar 1](screenshot/ss.png)
---
+++ luas dan keliling lingkaran.py
@@ -0,0 +1,9 @@
+print("program menghitung luas dan keliling lingkaran")
+r = int(input("masukkan jari - jari = "))
+
+phi = 22/7
+luas = phi*r*r
+keliling = 2*phi*r
+
+print("luas lingkaran adalah     = ", luas)
+print("keliling lingkaran adalah = ", keliling)
